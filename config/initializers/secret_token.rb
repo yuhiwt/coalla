7322e0f161454105c25915383e63923967cc915d
yuuy <iwata.yuhei@gmail.com>
2015-07-14 20:55:04 +0900

    initialize repositorygit status Hello gitgit status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '7e202bdb0e89c441698ec8ae6a55a1e96b02957d20bb4f4d3094d75ea63b85079fb0fc3fe3e688d24c0939267627fb06e6fed179d3e6540edc7f79623283c857'
